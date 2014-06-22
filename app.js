0a033f5e65fed179f99b9ce9b55fc4649590c528
denysboiko <denys.bojko@gmail.com>
2014-06-22 16:18:36 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var config = require('config');
+var log = require('libs/log')(module);
+
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+//app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+
+app.use(express.urlencoded());
+
+app.use(express.cookieParser());
+
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next) {
+    res.render("index", {
+        body: 'Голова'
+    });
+});
+
+app.use(function(err, req, res, next) {
+    // NODE_ENV = 'production'
+    if (app.get('env') == 'development') {
+        var errorHandler = express.errorHandler();
+        errorHandler(err, req, res, next);
+    } else {
+        res.send(500);
+    }
+});
+// development only
+/*
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var routes = require('./routes');
+var user = require('./routes/user');
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+
+ app.use(function(req, res, next) {
+ if (req.url == '/forbidden') {
+ next(new Error("wops, denied"));
+ } else {
+ next();
+ }
+ });
+
+ app.use(function(req, res, next) {
+ if (req.url == '/test') {
+ res.end("Test");
+ } else {
+ next();
+ }
+ });
+
+ app.use(function(req, res) {
+ res.send(404, "Page Not Found Sorry");
+ });
+
+ app.use(express.favicon());
+ app.use(express.logger('dev'));
+ app.use(express.json());
+ app.use(express.urlencoded());
+ app.use(express.methodOverride());
+ app.use(express.cookieParser('your secret here'));
+ app.use(express.session());
+ app.use(app.router);
+ app.use(express.static(path.join(__dirname, 'public')));
+
+
+*/
+
+http.createServer(app).listen(config.get('port'), function(){
+  log.info('Express server listening on port ' + app.get('port'));
+});
