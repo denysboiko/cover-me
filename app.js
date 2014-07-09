450de02007f4653053369be7b3bf84a5cab471d8
denysboiko <denysboiko@email.com>
2014-07-09 18:45:15 +0300

    coverme

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res, next) {
     res.render("index", {
-        body: 'Голова'
+        brand: 'Cover Me'
     });
 });
 
