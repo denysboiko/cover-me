adcd3dc25af456fc8fd1f6f43fb736c31f5e6bf5
akmnko <akmnko@gmail.com>
2014-07-10 19:00:43 +0300

    CreatingDb functions
    1. Cleaning the db
    2. Adding covers
    3. Closing

--- createDb.js
+++ createDb.js
@@ -1,17 +1,53 @@
-var Cover = require('models/cover').Cover;
+var mongoose = require('libs/mongoose');
+var async = require('async');
 
-var cover = new Cover({
+/*var cover = new Cover({
     artist: "Tim Minchin",
     album: "Darkside",
     year: "2008",
     sPicture: "smaaaal.jpg",
     bPicture: "biiiiig.jpg"
+});*/
+
+async.series([
+    open,
+    dropDatabase,
+    requireModels,
+    createCovers
+], function(err) {
+    console.log(arguments);
+    mongoose.disconnect();
+    process.exit(err ? 255 : 0);
 });
 
-cover.save(function(err, cover, affected) {
-    if (err) throw err;
+function open(callback) {
+    mongoose.connection.on('open', callback);
+}
 
-    Cover.findOne({album: "Darkside"}, function(err, cover) {
-        console.log(cover);
-    });
-});
+function dropDatabase(callback) {
+    var db = mongoose.connection.db;
+    db.dropDatabase(callback);
+}
+
+function requireModels(callback) {
+    require('models/cover');
+
+    async.each(Object.keys(mongoose.models), function(modelName, callback) {
+        mongoose.models[modelName].ensureIndexes(callback);
+    }, callback);
+}
+
+function createCovers(callback) {
+
+    var covers = [
+        {artist: 'Tim Minchin', album: 'Darkside', year: '2008', sPicture: 'smaaaal.jpg', bPicture: 'biiiiig.jpg'},
+        {artist: 'Oasis', album: 'SOTSOG', year: '2004', sPicture: 'smaaaal1.jpg', bPicture: 'biiiiig2.jpg'}
+    ];
+
+    console.log(covers);
+
+    async.each(covers, function(coverData, callback) {
+        var cover = new mongoose.models.Cover(coverData);
+        cover.save(callback);
+    }, callback);
+}
